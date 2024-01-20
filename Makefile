1bb5318e2df7f20fe247cece3443566471bdd6b0
manastria <manastria@users.noreply.github.com>
2024-01-20 18:43:28 +0100

    Prise en charge de plusieurs connexions

--- Makefile
+++ Makefile
@@ -1,16 +1,30 @@
+# Spécification du compilateur et des options de compilation
 CC=gcc
 CFLAGS=-Wall -Wextra -pedantic
+
+# Bibliothèques à lier, si nécessaire
 LIBS=
+
+# Répertoire d'installation des programmes
 INSTALL_DIR=~/bin
 
+# Nom du programme principal à compiler
 PROGRAM=netcat_server
-INSTALL_NAMES=myprogram1 myprogram2 myprogram3
 
+# Noms des programmes à installer.
+# Actuellement, seul le PROGRAM principal est installé.
+# Pour installer plusieurs programmes, décommentez et modifiez la ligne ci-dessous.
+# INSTALL_NAMES=myprogram1 myprogram2 myprogram3
+INSTALL_NAMES=$(PROGRAM)
+
+# Règle pour construire le programme principal
 all: $(PROGRAM)
 
+# Règle pour compiler le programme
 $(PROGRAM): $(PROGRAM).c
 	$(CC) $(CFLAGS) -o $(PROGRAM) $(PROGRAM).c $(LIBS)
 
+# Règle pour installer le ou les programmes
 install: $(PROGRAM)
 	@if [ ! -d $(INSTALL_DIR) ]; then mkdir -p $(INSTALL_DIR); fi
 	@for name in $(INSTALL_NAMES); do \
@@ -18,6 +32,7 @@ install: $(PROGRAM)
 		install $(PROGRAM) $(INSTALL_DIR)/$$name; \
 	done
 
+# Règle pour nettoyer les fichiers générés lors de la compilation
 .PHONY: clean
 clean:
 	rm -f $(PROGRAM)
--- netcat_server.c
+++ netcat_server.c
@@ -11,10 +11,24 @@
 #include <ifaddrs.h>
 #include <getopt.h>
 #include <netdb.h>
+#include <signal.h>
+#include <pthread.h>
 
 
 #define DEFAULT_PORT 3300
 #define BUFFER_SIZE 1024
+
+
+// Déclare le descripteur de fichier du socket comme variable globale
+int sockfd;
+
+void signal_handler(int sig) {
+    if (sig == SIGINT) {
+        printf("Received signal %d, closing server...\n", sig);
+        close(sockfd);
+        exit(0);
+    }
+}
 
 /**
  * Prints the usage instructions for the program.
@@ -52,6 +66,7 @@ void send_time_and_socket_info(int sockfd, struct sockaddr_in *clientaddr, sockl
         send(sockfd, buffer, strlen(buffer), 0);
     }
 }
+
 
 void display_ips() {
     struct ifaddrs *ifaddr, *ifa;
@@ -96,66 +111,129 @@ void display_ips() {
     freeifaddrs(ifaddr);
 }
 
-
-int main(int argc, char *argv[]) {
-    int sockfd;
-    bool is_udp = false;
-    int port = DEFAULT_PORT;
-    struct sockaddr_in serveraddr, clientaddr;
-    socklen_t clientlen = sizeof(clientaddr);
+void parse_arguments(int argc, char *argv[], bool *is_udp, int *port) {
     int c;
-
 
     while ((c = getopt(argc, argv, "tu:p:")) != -1) {
         switch (c) {
             case 't':
-                is_udp = false;
+                *is_udp = false;
                 break;
             case 'u':
-                is_udp = true;
+                *is_udp = true;
                 break;
             case 'p':
-                port = atoi(optarg);
+                *port = atoi(optarg);
                 break;
             default:
                 print_usage();
-                return 1;
+                exit(1);
         }
     }
+}
 
-    // Affiche les adresses IP
-    display_ips();
 
-    // Create the socket
-    sockfd = socket(AF_INET, is_udp ? SOCK_DGRAM : SOCK_STREAM, 0);
+int create_socket(bool is_udp, int port) {
+    int sockfd = socket(AF_INET, is_udp ? SOCK_DGRAM : SOCK_STREAM, 0);
     if (sockfd < 0) {
         perror("Erreur lors de la création du socket");
-        return 1;
+        exit(1);
     }
 
-    // Set up the server address structure
+    struct sockaddr_in serveraddr;
     memset(&serveraddr, 0, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
     serveraddr.sin_port = htons(port);
 
-    // Bind the socket to the server address
     if (bind(sockfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0) {
         perror("Erreur lors du bind");
-        return 1;
+        exit(1);
     }
 
-    // Start listening for incoming connections (TCP only)
     if (!is_udp && listen(sockfd, 5) < 0) {
         perror("Erreur lors de l'écoute");
-        return 1;
+        exit(1);
     }
 
+    return sockfd;
+}
+
+void handle_client_communication(int clientfd, bool is_udp, struct sockaddr_in clientaddr, socklen_t clientlen) {
+    char client_ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &clientaddr.sin_addr, client_ip, INET_ADDRSTRLEN);
+    int client_port = ntohs(clientaddr.sin_port);
+    printf("Received connexion from client %s:%d\n", client_ip, client_port);
+
+    while (1) {
+        char buffer[BUFFER_SIZE];
+        ssize_t n;
+
+        if (is_udp) {
+            n = recvfrom(clientfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&clientaddr, &clientlen);
+        } else {
+            n = recv(clientfd, buffer, BUFFER_SIZE, 0);
+        }
+
+        if (n <= 0) {
+            break;
+        }
+
+        buffer[n] = '\0';
+        char *trimmed_buffer = strtok(buffer, "\r\n");
+
+        if (strcmp(trimmed_buffer, "quit") == 0 || strcmp(trimmed_buffer, "exit") == 0) {
+            close(clientfd);
+            printf("Serveur arrêté.\n");
+            exit(0);
+        } else if (strcmp(trimmed_buffer, "bye") == 0) {
+            break;
+        } else {
+            printf("Received message from client %s:%d: %s\n", client_ip, client_port, trimmed_buffer);
+
+            time_t now = time(NULL);
+            struct tm tm_now = *localtime(&now);
+            char time_str[64];
+            strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", &tm_now);
+
+            char response[BUFFER_SIZE];
+            snprintf(response, sizeof(response), "%s | %s\n", time_str, trimmed_buffer);
+
+            if (is_udp) {
+                sendto(clientfd, response, strlen(response), 0, (struct sockaddr *)&clientaddr, clientlen);
+            } else {
+                send(clientfd, response, strlen(response), 0);
+            }
+
+            printf("Sent message to client %s:%d\n", client_ip, client_port);
+            printf("Sent message to client %s:%d: %s\n", client_ip, client_port, response);
+        }
+    }
+
+    if (!is_udp) {
+        close(clientfd);
+    }
+}
+
+void* client_handler(void* arg) {
+    int clientfd = *(int*)arg;
+    struct sockaddr_in clientaddr;
+    socklen_t clientlen = sizeof(clientaddr);
+    getpeername(clientfd, (struct sockaddr*)&clientaddr, &clientlen);
+    handle_client_communication(clientfd, true, clientaddr, clientlen);
+    close(clientfd);
+    pthread_exit(NULL);
+}
+
+void run_server(bool is_udp, int port) {
+    int sockfd = create_socket(is_udp, port);
     printf("Serveur en écoute sur le port %d...\n", port);
 
-    // Main server loop
     while (1) {
         int clientfd;
+        struct sockaddr_in clientaddr;
+        socklen_t clientlen = sizeof(clientaddr);
+
         if (is_udp) {
             clientfd = sockfd;
         } else {
@@ -166,78 +244,44 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        // Send the date, time, and socket parameters to the client
-        send_time_and_socket_info(clientfd, &clientaddr, clientlen, is_udp);
+        // Crée un thread pour gérer la connexion
+        pthread_t thread_id;
+        int* new_clientfd = malloc(sizeof(int));
+        *new_clientfd = clientfd;
 
-
-        // Get the client IP address and port
-        char client_ip[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &clientaddr.sin_addr, client_ip, INET_ADDRSTRLEN);
-        int client_port = ntohs(clientaddr.sin_port);
-        printf("Received connexion from client %s:%d\n", client_ip, client_port);
-
-        // Communication loop
-        while (1) {
-            char buffer[BUFFER_SIZE];
-            ssize_t n;
-
-            // Receive a message from the client
-            if (is_udp) {
-                n = recvfrom(clientfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&clientaddr, &clientlen);
-            } else {
-                n = recv(clientfd, buffer, BUFFER_SIZE, 0);
-            }
-
-            if (n <= 0) {
-                break;
-            }
-
-            
-
-            buffer[n] = '\0';
-            char *trimmed_buffer = strtok(buffer, "\r\n");
-
-            // Handle client commands
-            if (strcmp(trimmed_buffer, "quit") == 0 || strcmp(trimmed_buffer, "exit") == 0) {
-                close(clientfd);
-                close(sockfd);
-                printf("Serveur arrêté.\n");
-                return 0;
-            } else if (strcmp(trimmed_buffer, "bye") == 0) {
-                break;
-            } else {
-                // Print the received message
-                printf("Received message from client %s:%d: %s\n", client_ip, client_port, trimmed_buffer);
-
-                // Send the date and the received message back to the client
-                time_t now = time(NULL);
-                struct tm tm_now = *localtime(&now);
-                char time_str[64];
-                strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", &tm_now);
-
-                char response[BUFFER_SIZE];
-                snprintf(response, sizeof(response), "%s | %s\n", time_str, trimmed_buffer);
-
-                if (is_udp) {
-                    sendto(clientfd, response, strlen(response), 0, (struct sockaddr *)&clientaddr, clientlen);
-                } else {
-                    send(clientfd, response, strlen(response), 0);
-                }
-
-                printf("Sent message to client %s:%d\n", client_ip, client_port);
-                // Print the sent message
-                printf("Sent message to client %s:%d: %s\n", client_ip, client_port, response);
-            }
-        }
-
-        // Close the client socket (TCP only)
-        if (!is_udp) {
+        if (pthread_create(&thread_id, NULL, client_handler, (void*)new_clientfd) < 0) {
+            perror("Erreur lors de la création du thread");
             close(clientfd);
+            free(new_clientfd);
+            continue;
         }
+
+        // Détache le thread pour libérer les ressources
+        pthread_detach(thread_id);
     }
 
-    // Close the server socket
     close(sockfd);
-    return 0;
 }
 
+
+
+int main(int argc, char *argv[]) {
+    bool is_udp = false;
+    int port = DEFAULT_PORT;
+
+    // Enregistre le gestionnaire de signal pour le signal SIGINT (CTRL+C)
+    signal(SIGINT, signal_handler);
+
+    sockfd = create_socket(is_udp, port);
+
+    // Enregistre le gestionnaire de signal pour le signal SIGINT (CTRL+C)
+    signal(SIGINT, signal_handler);
+
+    sockfd = create_socket(is_udp, port);
+
+    parse_arguments(argc, argv, &is_udp, &port);
+    display_ips();
+    run_server(is_udp, port);
+
+    return 0;
+}
